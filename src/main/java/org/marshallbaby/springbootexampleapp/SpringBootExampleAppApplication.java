18994c360deaf7cba6bac20fa50ed3a7b4189b4d
Alexey Yatsevich <aleksey.yatsevich@gmail.com>
2024-05-17 14:29:53 +0200

    init skeleton

---
+++ src/main/java/org/marshallbaby/springbootexampleapp/SpringBootExampleAppApplication.java
@@ -0,0 +1,13 @@
+package org.marshallbaby.springbootexampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExampleAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/marshallbaby/springbootexampleapp/SpringBootExampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marshallbaby.springbootexampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
